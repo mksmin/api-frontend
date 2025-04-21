c041f2d6df663aa7f3c106b1969598b6d255c045
mks_min <mininname@vk.com>
2025-04-21 12:55:23 +0300

    Refactor: some improvements

--- src/scripts/auth.js
+++ src/scripts/auth.js
@@ -69,6 +69,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                     window.history.replaceState({}, document.title, path);
                     window.dynamicLoadContent();
                     window.hideResult();
+                    window.initDynamicContent()
                 } else {
                     elements.statusBlock.className = 'status-indicator status-error';
                     elements.statusBlock.textContent = `❌ Ошибка ${response.status}: ${data.message || 'Неизвестная ошибка'}`;
