394a2611387eca31b50702cf3ad3c258ae41f0e2
mks_min <mininname@vk.com>
2025-11-14 16:18:50 +0300

    feat: create telegram service

--- static/scripts/affirmations/services/telegram.js
+++ static/scripts/affirmations/services/telegram.js
@@ -1,1 +1,41 @@
-import
+import {CONFIG, MESSAGES} from "../config/constants.js";
+
+export class TelegramService {
+  constructor() {
+    this.tg = window.Telegram?.WebApp;
+    this.isInTelegram = !!this.tg && this.tg.initData !== '';
+
+    if (this.isInTelegram) {
+      this.tg.ready();
+      this.tg.expand();
+      console.log('TelegramService initialized in Telegram');
+    }
+    console.log('TelegramService initialized');
+  };
+
+  isAvailable() {
+    return this.isInTelegram;
+  }
+
+  showMainButton(text, onClick) {
+    if (!this.isInTelegram) return;
+    this.tg.MainButton.show();
+    this.tg.MainButton.setParams({
+      text,
+      has_shine_effect: false,
+      is_visible: true,
+      color: CONFIG.TELEGRAM.MAIN_BUTTON_COLOR,
+      position: 'left',
+    });
+    this.tg.MainButton.onClick(onClick);
+  }
+
+  hideMainButton(onClick) {
+    if (!this.isInTelegram) return;
+
+    if (onClick) {
+      this.tg.MainButton.offClick(onClick);
+    }
+    this.tg.MainButton.hide();
+  }
+}
