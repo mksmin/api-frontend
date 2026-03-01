c06a7d5279d5cd433b65d40052a06609b8802e7e
mks_min <mininname@vk.com>
2026-03-01 21:47:08 +0300

    feat: update miniapp auth

--- static/scripts/tgInitDataFetch.js
+++ static/scripts/tgInitDataFetch.js
@@ -1,10 +1,10 @@
 import {DOMUtils} from "./affirmations/utils/dom.js";
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const path = window.location.pathname;
-  const botName = path.split('/').pop();
+  const botAuthPath = document.querySelector('meta[name="miniapp-bot-auth-path"]');
+  let AUTH_PATH = "";
   const tg = Telegram.WebApp;
-  let AUTH_PATH = '/auth';
+  const errorsMeta = document.querySelector('meta[name="errors"]');
 
   const elements = {
     statusAuth: DOMUtils.getById('statusAuth'),
@@ -13,7 +13,13 @@ document.addEventListener('DOMContentLoaded', async () => {
     gradient: DOMUtils.query('.gradient'),
   }
 
-  function setAuthStatus(element, header = '', message = '', className = 'color-telegram') {
+  function setAuthStatus(
+    element,
+    header = '',
+    message = '',
+    subtext = "",
+    className = 'color-telegram',
+  ) {
     if (!element) {
       console.error('setAuthStatusError: элемент не найден');
       return;
@@ -22,10 +28,50 @@ document.addEventListener('DOMContentLoaded', async () => {
   <span class="word ${className}">${header}</span>
   <span class="word">${message}</span>
 `;
+
+    if (subtext) {
+      element.innerHTML += `
+      <span class="word mt-3 fs-3" style="line-height: normal">${subtext}</span>
+      `;
+    }
   }
 
-  if (path.includes('/apps/') && botName !== '') {
-    AUTH_PATH += `/${botName}`;
+  if (errorsMeta) {
+    const errors = errorsMeta.getAttribute('content');
+    setAuthStatus(
+      elements.statusAuth,
+      'Ошибка:',
+      errors,
+      "",
+      'text-error'
+    );
+    DOMUtils.addClass(elements.tgIcon, 'bg-color-error');
+    DOMUtils.addClass(elements.gradient, 'gradient-error');
+    DOMUtils.removeClass(elements.gradient, 'gradient-telegram');
+    errorsMeta.remove();
+    return;
+  }
+
+  if (botAuthPath) {
+    AUTH_PATH = botAuthPath.getAttribute('content');
+    botAuthPath.remove();
+  } else {
+    setAuthStatus(
+      elements.statusAuth,
+      'Ошибка:',
+      "Сервер не вернул путь авторизации",
+      "Попробуйте перезагрузить страницу или обратиться в поддержку",
+      'text-error'
+    );
+    DOMUtils.addClass(elements.tgIcon, 'bg-color-error');
+    DOMUtils.addClass(elements.gradient, 'gradient-error');
+    DOMUtils.removeClass(elements.gradient, 'gradient-telegram');
+
+    if (elements.botConfig) {
+      elements.botConfig.remove()
+    }
+
+    return;
   }
 
   try {
@@ -37,6 +83,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         elements.statusAuth,
         'Ошибка:',
         'не удалось получить данные от Telegram',
+        "",
         'text-warning'
       )
       DOMUtils.addClass(elements.tgIcon, 'bg-color-warning')
@@ -49,8 +96,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const response = await fetch(AUTH_PATH, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'X-Client-Source': 'TelegramMiniApp'
+        'Content-Type': 'application/x-www-form-urlencoded'
       },
       body: Telegram.WebApp.initData,
       credentials: 'include'
@@ -63,7 +109,9 @@ document.addEventListener('DOMContentLoaded', async () => {
       setAuthStatus(
         elements.statusAuth,
         'Успешно:',
-        'авторизация прошла'
+        'авторизация прошла',
+        "",
+        'color-telegram'
       )
       window.location.href = redirectUrl || '/profile';
     } else {
@@ -72,10 +120,10 @@ document.addEventListener('DOMContentLoaded', async () => {
         elements.statusAuth,
         'Ошибка:',
         `${response.status}, ${data.detail || JSON.stringify(data)}`,
+        "",
         'text-error'
       )
       DOMUtils.addClass(elements.tgIcon, 'bg-color-error')
-
       DOMUtils.addClass(elements.botConfig.querySelector('a'), 'bg-color-error')
       DOMUtils.addClass(elements.gradient, 'gradient-error')
       DOMUtils.removeClass(elements.gradient, 'gradient-telegram')
@@ -86,6 +134,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       elements.statusAuth,
       'Ошибка:',
       error.message,
+      "",
       'text-warning'
     )
     DOMUtils.addClass(elements.tgIcon, 'bg-color-warning')
